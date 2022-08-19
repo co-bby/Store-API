ce19d22525f2d0884f31afdd7b40ee0e0209a166
co-bby <nkumsagoefredrick@gmail.com>
2022-08-19 10:45:16 -0400

    set up mongoDB

--- app.js
+++ app.js
@@ -1,1 +1,38 @@
-console.log('04 Store API')
+require('dotenv').config();
+
+// async error
+const express = require('express');
+const app = express();
+
+const connectDB = require('./db/connect');
+
+const notFound = require('./middleware/not-found');
+const errorHandler = require('./middleware/error-handler');
+
+// middleware
+app.use(express.json());
+
+// routes
+app.get('/', (req, res) => {
+  res.send('<h1>Store API</h1><a href="/api/v1/products">products routes</a>');
+});
+
+app.use('/api/v1/products', products);
+// products routes
+app.use(errorHandler);
+app.use(notFound);
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    connectDB(process.env.MONGO_URI);
+    app.listen(port, () => {
+      console.log(`server is listening to port ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
